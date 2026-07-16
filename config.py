ddbe27c485bae78495187be1ed94ca112b8f7793
Sakshi <22f3002973@ds.study.iitm.ac.in>
2026-07-16 08:59:39 +0530

    Add configuration variables for email and API token

---
+++ config.py
@@ -0,0 +1,7 @@
+
+EMAIL = "22f3002973@ds.study.iitm.ac.in"
+AIPIPE_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6IjIyZjMwMDI5NzNAZHMuc3R1ZHkuaWl0bS5hYy5pbiIsImlhdCI6MTc4MzI1MTAyOCwiaXNzIjoiaHR0cHM6Ly9haXBpcGUub3JnIiwiYXVkIjoiYWlwaXBlLWFwaSIsImV4cCI6MTc4Mzg1NTgyOH0.SWei1Pozh2_b8vddYNdFhQA7zxBvfwkF8wG8aGwyls4"
+
+
+AIPIPE_BASE = "https://aipipe.org/openai/v1"
+TEXT_MODEL = "gpt-4o"
